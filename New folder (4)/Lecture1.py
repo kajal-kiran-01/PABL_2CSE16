09ca9e9f72c63e87514d2d8491f9e2defb178cb7
kajal-kiran-01 <kajal.kiran.cs28@iilm.edu>
2026-01-20 12:24:33 +0530

    Initial commit: Add project files

---
+++ New folder (4)/Lecture1.py
@@ -0,0 +1,10 @@
+per=int(input ("Enter your Percentage:"))
+if per>=60:
+    print("First Divi")
+elif per>=50:
+    print("2nd Divi")
+elif per>=40:
+    print("3rd divi")
+else:
+    print("you are fail")
+    
